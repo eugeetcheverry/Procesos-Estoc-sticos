12e858cbc35947e11663de604f360fb64b890608
María Eugenia Etcheverry <114704607+eugeetcheverry@users.noreply.github.com>
2024-09-02 11:44:01 -0300

    Add files via upload

--- ej2_repaso_senales.m
+++ ej2_repaso_senales.m
@@ -1,7 +1,7 @@
-% H(z) = (32z^3 -24z^2 -4z)/(32z^2)
+% H(z) = (8 - 6z^(-1) + z^(-2))/8
 
-b = [0, -4, -24, 32];
-a = [1, 0, 0, 32];
+b = [8, -6, -1];
+a = [8];
 figure;
 zplane(b, a);
 title('Diagrama polos y ceros de H(Z)');
